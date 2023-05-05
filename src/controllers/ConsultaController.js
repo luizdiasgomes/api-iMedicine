1265fa28d3ca3c8de1dc62e89c8717b497e07010
Luiz Fernando Dias Gomes <luizdiasgomes2@gmail.com>
2023-05-04 22:30:01 -0300

    bugfix again corrigindo detalhe query

--- src/controllers/ConsultaController.js
+++ src/controllers/ConsultaController.js
@@ -14,7 +14,7 @@ module.exports = {
         const { medico_id, pacientes_id, data_hora, tipo, observacao, clinica_id } = req.body;
 
         const [, affectRows] = await connection.query(`
-            INSERT INTO consulta VALUES (DEFAULT, ${medico_id}, ${pacientes_id}, '${data_hora}', '${tipo}', '${observacao}', ${clinica_id},)
+            INSERT INTO consulta VALUES (DEFAULT, ${medico_id}, ${pacientes_id}, '${data_hora}', '${tipo}', '${observacao}', ${clinica_id})
         `)
 
         response.success = affectRows > 0
